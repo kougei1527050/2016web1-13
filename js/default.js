3f136803b26c73f3bf2c206258c68416ecb07a4c
kougei1527050 <g1527050@st.t-kougei.ac.jp>
2016-07-04 18:02:25 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -10,6 +10,9 @@ var now =new Date();
 var target=new Date(2020,7,24,0,0,0,0);
 var diff=target.getTime() - now.getTime();
 var counter =separate_time(diff);
+document.getElementById('form').select.onchange=function(){
+  location.href=document.getElementById('form').select.value;
+}
 document.getElementById('countdown').textContent=
 '東京オリンピックまであと'+
 counter[3]+'日'+
@@ -23,7 +26,3 @@ var refresh=function(){
 }
 update();
 
-document.getElementById('form').select.onchange=function(){
-  location.href=document.getElementById('form').select.value;
-  
-}

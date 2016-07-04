38ec5735462cc410db8c1e4b623edc01cf49c54f
kougei1527050 <g1527050@st.t-kougei.ac.jp>
2016-07-04 18:22:29 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,15 +1,3 @@
-/*function getFileName(){
-  return window.location.href.split('/').pop();
-}
-var filename=getFileName();
-var opt;
-if(filename==='other.html'){
-  opt=document.querySelector('option[value="other.html]');
-}else{
-  opt=document.querySelector('option[value="index.html');
-}
-opt.selected=true;
-*/
 
 
 
